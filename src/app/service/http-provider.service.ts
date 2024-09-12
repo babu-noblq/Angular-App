738de78ef14a1d6b6316caf03f60fe8c9c1285a2
babu-noblq <179123670+babu-noblq@users.noreply.github.com>
2024-09-12 17:43:59 +0530

    Initial setup

---
+++ src/app/service/http-provider.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { WebApiService } from './web-api.service';
+
+//var apiUrl = "https://localhost:44370/";
+
+var apiUrl = "https://localhost:7037";
+
+var httpLink = {
+  getAllEmployee: apiUrl + "/api/Form",
+  deleteEmployeeById: apiUrl + "/api/employee/deleteEmployeeById",
+  getEmployeeDetailById: apiUrl + "/api/employee/getEmployeeDetailById",
+  saveEmployee: apiUrl + "/api/Form",
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpProviderService {
+
+  constructor(private webApiService: WebApiService) { }
+
+  public getAllEmployee(): Observable<any> {
+    return this.webApiService.get(httpLink.getAllEmployee);
+  }
+
+  public deleteEmployeeById(model: any): Observable<any> {
+    return this.webApiService.post(httpLink.deleteEmployeeById + '?employeeId=' + model, "");
+  }
+
+  public getEmployeeDetailById(model: any): Observable<any> {
+    return this.webApiService.get(httpLink.getEmployeeDetailById + '?employeeId=' + model);
+  }
+
+  public saveEmployee(model: any): Observable<any> {
+    return this.webApiService.post(httpLink.saveEmployee, model);
+  }
+  
+}
